3750535a0b1d651b6dcf722d33cb1c3398355e22
Fraptol <99817384+Fraptol@users.noreply.github.com>
2022-03-23 22:27:20 +0000

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,10 @@
+
+public class Print {
+    static String greet="Hello, ";
+    static String unknown="my friend";
+
+    public static String sayHello()
+    {
+        return greet+unknown+".";
+    }
+}
