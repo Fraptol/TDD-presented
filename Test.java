493cb2a4040c409f72f9d324ecb81c2b80b0af66
Fraptol <99817384+Fraptol@users.noreply.github.com>
2022-03-23 22:34:47 +0000

    fix

--- Test.java
+++ Test.java
@@ -1,21 +1,18 @@
 class Test {
-    static String greet="Hello, ";
-    static String unknown="my friend";
+    static String greet="Hello, "       ;
+    static String unknown="my friend"   ;
     public static void Try() {
-        String greetings =          "The note says: first run";
-        assert greetings.matches(   "The note says: first run");
-        System.out.println("Welcome to testing!");
+        String greetings =          "The note says: first run"  ;
+        assert greetings.matches(   "The note says: first run"  );
+        System.out.println("Welcome to testing!"                );
     }
     //1
     public static void TestHello1() {
         String greetings =          greet+unknown+"."   ;
-        System.out.println(         "Koszones 1..."     );
+        System.out.println(         "Koszones 1...")    ;
         assert greetings.matches(   "Hello my friend."  );
-        assert greetings.matches(   sayHello()          );
-}
-//Laying the groundworks,
-//Making sure it works.
-
+        assert greetings.matches(   Print.sayHello());
+    }
 }
 //Laying the groundworks,
 //Making sure it works.
