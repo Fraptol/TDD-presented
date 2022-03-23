a98700522a7d79d4e86010c8974e2a48a472aab7
Fraptol <99817384+Fraptol@users.noreply.github.com>
2022-03-23 22:17:46 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,16 @@
 class Test {
+    static String greet="Hello, ";
+    static String unknown="my friend";
     public static void Try() {
         String greetings =          "The note says: first run";
         assert greetings.matches(   "The note says: first run");
         System.out.println("Welcome to testing!");
+    }
+    //1
+    public static void TestHello1() {
+        String greetings =          greet+unknown+".";
+        System.out.println("Koszones 1...");
+        assert greetings.matches(   "Hello my friend.");
     }
 }
 //Laying the groundworks,
