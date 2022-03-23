cfe9eaeafdef74218c4150d940bcce8983c52fb2
Fraptol <99817384+Fraptol@users.noreply.github.com>
2022-03-23 21:09:17 +0000

    Teszt class létrehozása
    Mint első lépés, ellenőriztem egy egyszerű feladattal, hogy elméletben meg tudom-e oldani a feladatom.

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test {
+    public static void main (String args[])
+    {
+        String greetings = "The note says: first run";
+        assert greetings.matches("The note says: first run");
+    }
+}
+//Laying the groundworks,
+//Making sure it works.
