6036ab5bb0ed82295af67873b156c1efd9c2609b
Fraptol <99817384+Fraptol@users.noreply.github.com>
2022-03-23 22:49:48 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 class Test {
     static String greet="Hello, "       ;
     static String unknown="my friend"   ;
+    static String and=", and "          ;
     public static void Try() {
         String greetings =          "The note says: first run"  ;
         assert greetings.matches(   "The note says: first run"  );
@@ -19,6 +20,14 @@ class Test {
         System.out.println(         "Koszones nev szerint..."   );
         assert greetings.matches(   Print.sayHello("Bob")       );
     }
+    //3
+    public static void TestHello3_aaa(){
+        String greetings =          greet+"Alice, Bob"+and+"Jerry." ;
+        System.out.println(         "3 koszones nev szerint..."       );
+        assert greetings.matches(   Print.sayHello("Bob")           );
+    }
+    }        
 }
 //Laying the groundworks,
-//Making sure it works.
+//Making sure it works,
+//Refractoring until everything is up to step.
