b88fd41984cd42d711c1d47239dccf02d255b04a
Fraptol <99817384+Fraptol@users.noreply.github.com>
2022-03-23 22:09:18 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public static void main(String[] args) {
+    //0
+    Try();
+}
+
+
+/*
+tervezett lépések
+//1
+assertThat (Print.sayHello(), is "Hello, my friend.");
+//2
+Scan.hearNames("Hi");
+assertThat (Print.sayHello("Hi"), is "Hello, Hi.");
+assertThat (Print.sayHello(scan.current), is "Hello, Hi.");
+//3
+Scan.hearNames("Jonas");
+assertThat (Print.sayHello(scan.current), is "Hello, Hi and Jonas.");
+Scan.forgetNames();
+assertThat (Print.sayHello(scan.current), is "Hello, my friend.");
+//4
+Scan.hearNames("ANGER");
+assertThat (Print.sayHello(scan.current), is "HELLO, ANGER.");
+//5
+Scan.hearNames("Calmer");
+assertThat (Print.sayHello(scan.current), is "Hello, Calmer. AND HELLO, ANGER");
+//6
+Scan.hearNames("Bela, Charlotte, KENNEDY, VIT");
+assertThat (Print.sayHello(scan.current), is "Hello, Calmer, Bela and Charlotte. AND HELLO ANGER, KENNEDY AND VIT.");
+*/
+
+/*
+String felépítések:
+
+"Hello my friend."
+"Hello, "+ +"."
+"Hello, "+ +" and "+ +"."
+
+"HELLO "+ +"!"
+"HELLO "+ +" AND "+"!"
+
+"AND HELLO "
+
+Például megadva egyszerre Alaura, Baeüler legyen 2 külön köszöntött név
+*/
